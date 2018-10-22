84b7e33c4223a2b04de1ed4839a4513be2bfa085
o(￣へ￣)o <514668826@qq.com>
2018-10-22 18:07:36 +0800

    em...

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   has_many :todos, foreign_key: :created_by, dependent: :destroy
   # Validations
   validates :name, :email, :password_digest, presence: true
+  # validates :email,
+  #           uniqueness: true,
+  #           format: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+  #           on: :create
 end
